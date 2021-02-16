0b7125f8f52a542bb1d1a352645890a78cad5d7d
WakiAki <65297381+WakiAki@users.noreply.github.com>
2021-02-16 17:21:23 +0000

    Drew an orange rectangle

--- HelloGL/HelloGL.cpp
+++ HelloGL/HelloGL.cpp
@@ -7,7 +7,7 @@ HelloGL::HelloGL(int argc, char* argv[])
 	glutInitWindowSize(800, 800);
 
 	//name of the window
-	glutCreateWindow("Simple Window");
+	glutCreateWindow("Drawing Polygons");
 	glutDisplayFunc(GLUTcallbacks::Display);
 	glutMainLoop();
 }
@@ -18,5 +18,13 @@ HelloGL::~HelloGL(void)
 
 void HelloGL::Display()
 {
-
+	glClear(GL_COLOR_BUFFER_BIT); //Clears the scene
+	glBegin(GL_POLYGON);// starts the draw process
+		glColor3f(1.0f, 0.5f, 0.25f);
+		glVertex2f(-0.75, 0.5);
+		glVertex2f(0.75, 0.5);
+		glVertex2f(0.75, -0.5);
+		glVertex2f(-0.75, -0.5);
+		glEnd(); // defines the end of the draw process 
+	glFlush(); //flushes the scene drawn to the graphics card
 }
